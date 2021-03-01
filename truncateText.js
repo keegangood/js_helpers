cb337bd32c31d544e3a8859e761f43228ede7902
unknown <keegood8@gmail.com>
2021-02-28 21:52:35 -0800

    added docstring

--- truncateText.js
+++ truncateText.js
@@ -1,3 +1,11 @@
+/**
+Takes in text and the maximum characters to show before replacing 
+the rest with an ellipsis...
+
+Returns the max number of characters followed by an ellipsis
+ * @param {string} text - Text to be truncated
+ * @param {number} max - Default: 10 - Number of characters to show in truncated text
+ */
 export default (text, max=10) => {
   return text.length < max ? text : text.substr(0,max) + '...'
 }
